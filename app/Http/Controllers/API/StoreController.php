964b2f89db7a87bac98c9061c4f1c78feabadfed
Kuroosh <kurooshptl@gmail.com>
2021-10-22 08:00:21 +0200

    Changed Store Cron

--- app/Http/Controllers/API/StoreController.php
+++ app/Http/Controllers/API/StoreController.php
@@ -22,7 +22,7 @@ class StoreController extends Controller
      */
     public function __construct()
     {
-//        $this->middleware('auth:api');
+        $this->middleware('auth:api');
     }
 
     /**
